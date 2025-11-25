09c3372ad80fdaad769cd8ef92459c8a2eb1a748
Adryan Galdino <adry4nbr@gmail.com>
2025-11-25 19:04:54 -0300

    Tele-Agendamento 2.0

--- Agendamento.py
+++ Agendamento.py
@@ -1,41 +1,99 @@
 from collections import deque
+from time import sleep
 
 class Agendamento:
-  def add(self, nome, CPF, c_sus, c_residencia, fila_externa):
-    self.nome = nome
-    self.c_sus = c_sus
-    self.c_residencia = c_residencia
-    self.CPF = CPF
-    fila_externa.append(self)
-    print(f"{self.nome} agora está na fila")
+    fila = deque()
+    proximo_id = 1
 
-  def __str__(self):
-    return f"Agendamento(Nome: {self.nome}, CPF: {self.CPF}, C_SUS: {self.c_sus}, C_Residencia: {self.c_residencia})"
+    def __init__(self, nome, cpf, c_sus, c_residencia):
+        self.cpf = cpf
+        self.agendamento_id = Agendamento.proximo_id
+        self.nome = nome
+        self.c_sus = c_sus
+        self.c_residencia = c_residencia
+        Agendamento.proximo_id += 1
 
-  def mostrar_fila(self):
-    if fila_global:
-      print("\n fila atual:",list(fila_global))
-    else:
-      print("\n Nenhuma pessoa na fila")
+    def __str__(self):
+        return f"Agendamento {self.agendamento_id} (Nome: {self.nome}, CPF: {self.cpf}, C_SUS: {self.c_sus}, C_Residencia: {self.c_residencia})"
 
-fila_global = deque()
+    @classmethod
+    def add(cls, nome, cpf, c_sus, c_residencia):
+        novo_agendamento = cls(nome, cpf, c_sus, c_residencia)
+        cls.fila.append(novo_agendamento)
+        sleep(1.5)
+        print(f'{novo_agendamento.nome} foi agendado com sucesso!')
+        sleep(1.5)
+
+    @classmethod
+    def mostrar_fila(cls):
+        if cls.fila:
+            for fila_agendamento in cls.fila:
+                print(fila_agendamento)
+        else:
+            print('Nenhuma fila de agendamento')
+
+
+    @classmethod
+    def checar_agendamento(cls, numero_agendamento):
+        if not cls.fila:
+            print(f'Não há agendamentos marcados.')
+            print('='*30)
+            sleep(1.5)
+            return
+        elif cls.fila:
+            for fila_agendamento in cls.fila:
+                if not fila_agendamento.agendamento_id == numero_agendamento:
+                    print(f'Não foi encontrado nenhum agendamento com o ID indicado.')
+                    print('='*30)
+                    sleep(1.5)
+                    return
+        else:
+            for fila_agendamento in cls.fila:
+                if fila_agendamento.agendamento_id == numero_agendamento:
+                    print(fila_agendamento)
+                    print('='*30)
+                    sleep(1.5)
+                    return
+
+#p1 = Agendamento.add("Fernando da Silva", 1111111, 1111111, 1111111)
 
 while True:
+    print("--------- Menu Principal ---------")
+    print("1. Fazer agendamento")
+    print("2. Checar agendamento")
+    print("3. Mostrar fila de clientes")
+    print("0. Sair do sistema")
 
-  gerenciador_agendamentos = Agendamento()
+    opcao = int(input("Escolha a opção: ").strip())
 
-  entrada_nome = input("Digite seu nome: ")
-  entrada_CPF = input("Digte seu CPF: ")
-  entrada_c_sus = input("Digite seu C_SUS: ")
-  entrada_c_residencia = input("Digite seu C_RESIDENCIA: ")
+    if opcao == 1:
+        print('-'*30)
+        print(f'Area de agendamento. Preencha os dados a seguir.')
+        nomeInput = str(input("Nome: "))
+        cpfInput = int(input("CPF (apenas números): "))
+        c_susInput = int(input("Número do cartão do SUS (apenas números): "))
+        c_residenciaInput = int(input("Número do cartão de residencia (apenas números): "))
+        print('-'*30)
+        Agendamento.add(nomeInput, cpfInput, c_susInput, c_residenciaInput)
 
-  gerenciador_agendamentos.add(entrada_nome, entrada_CPF, entrada_c_sus, entrada_c_residencia, fila_global)
+    elif opcao == 2:
+        print('='*30)
+        sleep(1)
+        print(f'Qual o ID de agendamento que deseja buscar?')
+        numero_agendamento = int(input("ID do agendamento: "))
+        print('='*30)
+        sleep(1.5)
+        Agendamento.checar_agendamento(numero_agendamento)
 
-  print("Estado atual da fila:")
-  for mostrar_fila in fila_global:
-      print(mostrar_fila)
+    elif opcao == 3:
+        print('-'*30)
+        sleep(1.5)
+        Agendamento.mostrar_fila()
+        sleep(1.5)
 
-  outro_agendamento = input("Deseja adicionar outra pessoa? (s/n)")
-  if outro_agendamento != "s":
-    print("Encerrando, até a proxima")
-    break
+    elif opcao == 0:
+        print("Encerrando o sistema. Até logo")
+        break
+
+    else:
+        print("Opção invalida tente novamente.")
